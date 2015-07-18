9a21a657a997d8264b0a7fa6fab3f4f49d891240
Pieter Pareit <pieter.pareit@gmail.com>
2015-07-18 08:45:36 +0200

    Implemented colored rectangles

--- app/src/main/java/be/ppareit/modernartui/MainActivity.java
+++ app/src/main/java/be/ppareit/modernartui/MainActivity.java
@@ -1,16 +1,52 @@
 package be.ppareit.modernartui;
 
+import android.graphics.Color;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.Random;
+import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private LinearLayout mRootLinearLayout;
+    private Random random = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRootLinearLayout = (LinearLayout) findViewById(R.id.root_linearlayout);
+        fillLinearLayout(mRootLinearLayout, 4);
+    }
+
+    private void fillLinearLayout(LinearLayout parent, int depth) {
+        if (depth == 0) {
+            return;
+        }
+        for (int i = 0; i < depth + random.nextInt(2); ++i) {
+            LinearLayout linLay = new LinearLayout(this);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            lp.weight = 1 + random.nextInt(2);
+            if ( depth == 1) {
+                lp.setMargins(2, 2, 2, 2);
+                int color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+                linLay.setBackgroundColor(color);
+            }
+            linLay.setLayoutParams(lp);
+            linLay.setOrientation(depth % 2 == 0 ? LinearLayout.HORIZONTAL : LinearLayout.VERTICAL);
+            parent.addView(linLay);
+            fillLinearLayout(linLay, depth - 1);
+        }
     }
 
 
